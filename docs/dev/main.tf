90a3b7107233c9a307c494ed1863fa382d5f5fba
hobittinen <144233792+hobittinen@users.noreply.github.com>
2026-03-02 21:05:25 +0200

    blueteam/docs/devs/opentofu/main.tf

---
+++ docs/dev/main.tf
@@ -0,0 +1,147 @@
+terraform {
+  required_providers {
+    null = { source = "hashicorp/null" }
+  }
+}
+
+provider "null" {}
+
+# Rakenna OVA-polut: absolute + Windowsin \ -> /
+locals {
+  ubuntu_ova  = replace(abspath("${path.module}/images/testikone.ova"), "\\", "/")
+  windows_ova = replace(abspath("${path.module}/images/konetesti.ova"), "\\", "/")
+}
+
+# Yhteinen import-skripti (heredoc HCL:lle, ei viittauksia destroyssa tähän!)
+locals {
+  import_ps = <<-PS
+    $name = $env:NAME
+    $ova  = $env:OVA
+
+    & VBoxManage showvminfo "$name" *> $null
+    if ($LASTEXITCODE -ne 0) {
+      Write-Host "Importing $name ..."
+      & VBoxManage import "$ova" --vsys 0 --vmname "$name"
+      exit $LASTEXITCODE
+    } else {
+      Write-Host "$name already exists. Skipping import."
+    }
+  PS
+}
+
+###############################
+# Ansible (Ubuntu)
+###############################
+resource "null_resource" "ansible" {
+  triggers = {
+    name = "Ansible-Controller"
+    ova  = local.ubuntu_ova
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = local.import_ps
+    environment = {
+      NAME = "Ansible-Controller"
+      OVA  = local.ubuntu_ova
+    }
+  }
+
+  # Destroy: ei viittauksia localsiin -> suora heredoc per resource
+  provisioner "local-exec" {
+    when        = destroy
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = <<-PS
+      $name = "Ansible-Controller"
+      & VBoxManage unregistervm "$name" --delete
+      exit 0
+    PS
+  }
+}
+
+###############################
+# Logging (Ubuntu)
+###############################
+resource "null_resource" "logging" {
+  triggers = {
+    name = "Logging-Server"
+    ova  = local.ubuntu_ova
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = local.import_ps
+    environment = {
+      NAME = "Logging-Server"
+      OVA  = local.ubuntu_ova
+    }
+  }
+
+  provisioner "local-exec" {
+    when        = destroy
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = <<-PS
+      $name = "Logging-Server"
+      & VBoxManage unregistervm "$name" --delete
+      exit 0
+    PS
+  }
+}
+
+###############################
+# Windows Server
+###############################
+resource "null_resource" "winserver" {
+  triggers = {
+    name = "Windows-Server"
+    ova  = local.windows_ova
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = local.import_ps
+    environment = {
+      NAME = "Windows-Server"
+      OVA  = local.windows_ova
+    }
+  }
+
+  provisioner "local-exec" {
+    when        = destroy
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = <<-PS
+      $name = "Windows-Server"
+      & VBoxManage unregistervm "$name" --delete
+      exit 0
+    PS
+  }
+}
+
+###############################
+# Windows Client
+###############################
+resource "null_resource" "winclient" {
+  triggers = {
+    name = "Windows-Client"
+    ova  = local.windows_ova
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = local.import_ps
+    environment = {
+      NAME = "Windows-Client"
+      OVA  = local.windows_ova
+    }
+  }
+
+  provisioner "local-exec" {
+    when        = destroy
+    interpreter = ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command"]
+    command     = <<-PS
+      $name = "Windows-Client"
+      & VBoxManage unregistervm "$name" --delete
+      exit 0
+    PS
+  }
+}
