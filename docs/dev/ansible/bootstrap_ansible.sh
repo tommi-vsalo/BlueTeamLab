fd1bd8416e86ca2876559c5b3a7e165822220836
tommi-vsalo <tommi.viljami.salo@gmail.com>
2026-03-19 19:04:24 +0200

    Update bootstrap_ansible.sh

--- docs/dev/ansible/bootstrap_ansible.sh
+++ docs/dev/ansible/bootstrap_ansible.sh
@@ -3,7 +3,9 @@ set -e
 
 sudo apt update
 sudo apt install -y ansible python3-pip
-python3 -m pip install --user pywinrm
+
+sudo apt install -y python3-winrm
+
 ansible-galaxy collection install ansible.windows community.windows
 
 mkdir -p ansible-controller/{playbooks,inventory}
